231fbf279652ed7f97fbad2f8de0e91c048ac854
Patrick5858 <patomaina2002@gmail.com>
2024-01-22 15:54:49 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+
+print("(The value of x is , x)")
+print ('This is a \n'
+       'check out this')
+# Integers
+x = 5
+print('x is a type',type(x),'\n')
+y = 1.234
+print('x is a type',type(y), '\n')
+z = 1.2333333345678
+print('x is a type',type(z), '\n')
+k = True
+print('x is a type',type(k),'\n')
+r = 'This is Kenya'
+print('x is a type',type(r),'\n')
+# List
+mylist1 = ['Jane','John','Kim']
+print(type(mylist1))
+print(mylist1)
+#Access Items
+print(mylist1[0])
+#Negative Indexing
+print(mylist1[-1])
+print(mylist1[0:2])
+mylist2 = ['Kirinyaga', 'Embu','Nyeri','Kiambu', 'Nakuru']
+print(mylist2[2:4])
