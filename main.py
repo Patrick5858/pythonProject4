721ff904a19565b18eac42a3cc414f1d7a84b9b0
Patrick5858 <patomaina2002@gmail.com>
2024-02-13 18:12:55 +0300

    new changes

--- main.py
+++ main.py
@@ -24,3 +24,64 @@ print(mylist1[-1])
 print(mylist1[0:2])
 mylist2 = ['Kirinyaga', 'Embu','Nyeri','Kiambu', 'Nakuru']
 print(mylist2[2:4])
+#Tuples
+mylist1.append('Rose')
+print(mylist1)
+
+print(len(mylist1))
+#1D Array
+myarray = [1,3,4,5,6]
+print(type(myarray))
+#2D array
+myarray2 =[[1,3,4,5,6],
+           ['f','h','y','w']]
+print(type(myarray2))
+#dictionary
+mydictionary ={
+       "Programe":"PA100",
+"Age":23,
+"County":"Embu"
+         }
+print(type(mydictionary))
+print(mydictionary['Age'])
+print(mydictionary['County'])
+mydictionary.get('Age')
+print(mydictionary.get('Age'))
+mydictionary['Age']=34
+print(mydictionary)
+
+
+v1 =[3,5,6,7]
+v2 = [9,3,5,2]
+a1 =v1,v2
+
+print(a1)
+results =[x*y for x,y in zip(v1,v2)]
+print(results)
+w =9
+y = 3
+print(w>y)
+print(w<y)
+print(v1<v2)
+print((w!=y))
+
+#control structure
+# If... else
+years =23
+if years >=18:
+    print('You are an adult')
+    print('you have', years, 'Years.')
+else:
+
+    print('You are an adult')
+    print('you have', years, 'Years.')
+n=int(input('Enter a number')
+if n>=0:
+    print('n',"is a positive number")
+elif n==0:
+    print(n, "is zero")
+else:
+    print(n,"is a negative number")
+)
+
+
